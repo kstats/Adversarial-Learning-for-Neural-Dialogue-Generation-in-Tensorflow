Changed adversarial training and fixed softmax bug

gen/gen_model.py
@@ -116,7 +116,7 @@ class Seq2SeqModel(object):
             
            
             #If we use output projection, we need to project outputs for decoding.
-            if output_projection is not None:
+            if self.output_projection is not None:
                 for b in xrange(len(buckets)):
                     self.outputs[b] = [tf.cond(self.forward_only, 
                                         lambda : tf.matmul(output, self.output_projection[0]) + self.output_projection[1],

gen/generator.py
@@ -77,7 +77,7 @@ def create_model(session, gen_config, forward_only):
 
 
 def softmax(x):
-    return np.exp(mid) / np.sum(np.exp(x), axis=0)
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
 
 
 def train(gen_config):
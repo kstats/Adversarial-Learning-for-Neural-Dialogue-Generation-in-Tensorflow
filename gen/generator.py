f535dd9f605fa95767a3a2297dd82c24e27a594f
kstats <katstasaski@gmail.com>
2017-04-30 02:12:26 -0700

    Merged katia's changes

--- gen/generator.py
+++ gen/generator.py
@@ -35,25 +35,25 @@ def read_data(dataset, max_size=None):
             break
         source_ids = dataset['context'][i]
         target_ids = dataset['response'][i]
-        #import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
 
         target_ids.append(data_utils.EOS_ID)
-        for bucket_id, (source_size, target_size) in enumerate(_buckets): 
+        for bucket_id, (source_size, target_size) in enumerate(_buckets):
             if len(source_ids) < source_size and len(target_ids) < target_size:
                 data_set[bucket_id].append([source_ids, target_ids])
                 break
     return data_set
 
-def prepare_data(gen_config):
-    
-    train_path                  = os.path.join(gen_config.data_dir, gen_config.train_data_file)
-    vocab, rev_vocab            = data_utils.initialize_vocabulary(gen_config.vocab_path)
 
-    dataset                     = data_utils.create_dataset(train_path, is_disc = False)
-    train_dataset, dev_dataset  = data_utils.split_dataset(dataset, ratio = gen_config.train_ratio )
+def prepare_data(gen_config):
+    train_path = os.path.join(gen_config.data_dir, gen_config.train_data_file)
+    vocab, rev_vocab = data_utils.initialize_vocabulary(gen_config.vocab_path)
+
+    dataset = data_utils.create_dataset(train_path, is_disc=False)
+    train_dataset, dev_dataset = data_utils.split_dataset(dataset, ratio=gen_config.train_ratio)
 
     # Read data into buckets and compute their sizes.
-    print ("Reading development and training data (limit: %d)." % gen_config.max_train_data_size)
+    print("Reading development and training data (limit: %d)." % gen_config.max_train_data_size)
     train_set, dev_set = read_data(train_dataset, gen_config.max_train_data_size), read_data(dev_dataset)
 
     return vocab, rev_vocab, dev_set, train_set
@@ -62,9 +62,9 @@ def prepare_data(gen_config):
 def create_model(session, gen_config):
     """Create generation model and initialize or load parameters in session."""
     model = seq2seq_model.Seq2SeqModel(
-                gen_config.vocab_size, gen_config.vocab_size, _buckets,
-                gen_config.size, gen_config.num_layers, gen_config.max_gradient_norm, gen_config.batch_size,
-                gen_config.learning_rate, gen_config.learning_rate_decay_factor, keep_prob=gen_config.keep_prob)
+        gen_config.vocab_size, gen_config.vocab_size, _buckets,
+        gen_config.size, gen_config.num_layers, gen_config.max_gradient_norm, gen_config.batch_size,
+        gen_config.learning_rate, gen_config.learning_rate_decay_factor, keep_prob=gen_config.keep_prob)
 
     ckpt = tf.train.get_checkpoint_state(gen_config.train_dir)
     if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
@@ -73,7 +73,7 @@ def create_model(session, gen_config):
     else:
         print("Created Gen_RNN model with fresh parameters.")
         session.run(tf.global_variables_initializer())
-    return model
+        return model
 
 
 def softmax(x):
@@ -85,53 +85,61 @@ def train(gen_config):
 
     with tf.Session() as sess:
         # Create model.
-        train_bucket_sizes  = [len(train_set[b]) for b in xrange(len(_buckets))]
-        train_total_size    = float(sum(train_bucket_sizes))
+        train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
+        train_total_size = float(sum(train_bucket_sizes))
         train_buckets_scale = [sum(train_bucket_sizes[:i + 1]) / train_total_size
                                for i in xrange(len(train_bucket_sizes))]
         # import pdb; pdb.set_trace()
         print("Creating %d layers of %d units." % (gen_config.num_layers, gen_config.size))
+        start_time = time.time()
         model = create_model(sess, gen_config)
-
+        end_time = time.time()
+        print("Time to create Gen_RNN model: %.2f" % (end_time - start_time))
 
         # This is the training loop.
         step_time, loss = 0.0, 0.0
         moving_average_loss = 0.0
-        current_step    = 0
+        current_step = 0
         previous_losses = []
 
         step_loss_summary = tf.Summary()
-        writer            = tf.summary.FileWriter("../logs/", sess.graph)
+        writer = tf.summary.FileWriter("../logs/", sess.graph)
 
         while True:
             # Choose a bucket according to data distribution. We pick a random number
             # in [0, 1] and use the corresponding interval in train_buckets_scale.
             random_number_01 = np.random.random_sample()
-            bucket_id           = min([i for i in xrange(len(train_buckets_scale))
-                           if train_buckets_scale[i] > random_number_01])
+            bucket_id = min([i for i in xrange(len(train_buckets_scale)) if train_buckets_scale[i] > random_number_01])
 
             # Get a batch and make a step.
             start_time = time.time()
+            # import pdb; pdb.set_trace()
+
             encoder_inputs, decoder_inputs, target_weights, batch_source_encoder, batch_source_decoder = model.get_batch(
                 train_set, bucket_id, 0)
 
-            _, step_loss, _ = model.step(sess, encoder_inputs, decoder_inputs, target_weights, bucket_id, forward_only = False)
+            _, step_loss, _ = model.step(sess, encoder_inputs, decoder_inputs, target_weights, bucket_id,
+                                         forward_only=False)
 
             step_time += (time.time() - start_time) / gen_config.steps_per_checkpoint
             loss += step_loss / gen_config.steps_per_checkpoint
             moving_average_loss += step_loss
             current_step += 1
 
-            if current_step % 150 == 0:
-              #sess.run(model.learning_rate_decay_op_one)
-              sample_context, sample_response, sample_labels, responses = gen_sample(sess, gen_config, model, vocab,
-                                               batch_source_encoder, batch_source_decoder, mc_search=False)
-              print("Step %d loss is %f, learning rate is %f" % (model.global_step.eval(), moving_average_loss / 150, model.learning_rate.eval()))
-              moving_average_loss = 0.0
-              print("Sampled generator:\n")
-              for input, response, label in zip(sample_context, sample_response, sample_labels):
-                print(str(label) + "\t" + str(input) + "\t" + str(response))
-            
+            if current_step % gen_config.steps_per_sample == 0:
+
+                sample_context, sample_response, sample_labels, responses = gen_sample(sess, gen_config, model, vocab,
+                                                                                       batch_source_encoder,
+                                                                                       batch_source_decoder,
+                                                                                       mc_search=False)
+                print("Step %d loss is %f, learning rate is %f" % (
+                model.global_step.eval(), moving_average_loss / 150, model.learning_rate.eval()))
+                moving_average_loss = 0.0
+                print("Sampled generator:\n")
+                for input, response, label in zip(sample_context, sample_response, sample_labels):
+                    print(str(label) + "\t" + str(input) + "\t" + str(response))
+                    # sess.run(model.learning_rate_interval_op_one)
+
             # Once in a while, we save checkpoint, print statistics, and run evals.
             if current_step % gen_config.steps_per_checkpoint == 0:
 
@@ -139,20 +147,17 @@ def train(gen_config):
                 bucket_value.tag = "loss"
                 bucket_value.simple_value = float(loss)
                 writer.add_summary(step_loss_summary, current_step)
-                write_steps = model.global_step.eval()
+
                 # Print statistics for the previous epoch.
-                lr = model.learning_rate.eval()
                 perplexity = math.exp(loss) if loss < 300 else float('inf')
-                print ("global step %d learning rate %.4f step-time %.2f perplexity "
-                       "%.6f" % (write_steps, lr,
-                                 step_time, perplexity))
+                print("global step %d learning rate %.4f step-time %.2f perplexity "
+                      "%.6f" % (model.global_step.eval(), model.learning_rate.eval(),
+                                step_time, perplexity))
                 # Decrease learning rate if no improvement was seen over last 3 times.
-                #if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
-                 #   sess.run(model.learning_rate_decay_op)
-                if write_steps > 200000 and lr != 0.05:
-                    sess.run(model.learning_rate_decay_op_two)
-                if write_steps < 200000 and write_steps > 100000 and lr != 0.1:
-                    sess.run(model.learning_rate_decay_op_one)
+                step_tracker = model.global_step.eval()
+
+                if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
+                    sess.run(model.learning_rate_decay_op)
                 previous_losses.append(loss)
                 # Save checkpoint and zero timer and loss.
                 checkpoint_path = os.path.join(gen_config.train_dir, "chitchat.model")
@@ -160,29 +165,31 @@ def train(gen_config):
                 step_time, loss = 0.0, 0.0
                 sys.stdout.flush()
 
+
 def get_predicted_sentence(sess, input_token_ids, vocab, model,
-                            beam_size, buckets, mc_search=True,debug=False):
-    
+                           beam_size, buckets, mc_search=True, debug=False):
     def model_step(enc_inp, dec_inp, dptr, target_weights, bucket_id):
-        _, _, logits  = model.step(sess, enc_inp, dec_inp, target_weights, bucket_id, forward_only = True)
-        prob          = softmax(logits[dptr][0])
+        _, _, logits = model.step(sess, enc_inp, dec_inp, target_weights, bucket_id, forward_only=True)
+        prob = softmax(logits[dptr][0])
         return prob
 
     def greedy_dec(output_logits):
-        import pdb; pdb.set_trace()
-      #  import pdb; pdb.set_trace()
-        selected_token_ids = [int(np.argmax(logit, axis=0)) for logit in np.squeeze(output_logits)]
-        for idx in range(len(selected_token_ids) - 1):
-            if idx == 0:
-                continue
-            if selected_token_ids[idx] == data_utils.EOS_ID:
-                selected_token_ids = selected_token_ids[:idx + 1]
-                break
+        # output_logits is [max_len X batch X vocab_size] ->
+        # transpose to [batch X max_len X vocab_size]
+        selected_token_ids = []
+        for logits in np.transpose(output_logits, (1, 0, 2)):
+            selected_token_ids.append([int(np.argmax(logit, axis=0)) for logit in logits])
+
+            #     import pdb; pdb.set_trace()
+        for b_id, s_t_id in enumerate(selected_token_ids):
+            eos_id = np.where(np.asarray(s_t_id) == data_utils.EOS_ID)
+            selected_token_ids[b_id] = s_t_id if len(eos_id[0]) == 0 else s_t_id[:np.min(eos_id[0]) + 1]
+
         return selected_token_ids
 
     # Which bucket does it belong to?
     bucket_id = min([b for b in range(len(buckets)) if buckets[b][0] > len(input_token_ids)])
-    outputs   = []
+    outputs = []
     feed_data = {bucket_id: [(input_token_ids, outputs)]}
 
     # Get a 1-element batch to feed the sentence to the model.   None,bucket_id, True
@@ -191,61 +198,64 @@ def get_predicted_sentence(sess, input_token_ids, vocab, model,
 
     ### Original greedy decoding
     if beam_size == 1 or (not mc_search):
-        _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs, target_weights, bucket_id, forward_only = True)
+        _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs, target_weights, bucket_id,
+                                         forward_only=True)
         return [{"dec_inp": greedy_dec(output_logits), 'prob': 1}]
 
     # Get output logits for the setence. # initialize beams as (log_prob, empty_string, eos)
-    beams, new_beams, results = [(1, {'eos': 0, 'dec_inp': decoder_inputs, 'prob': 1, 'prob_ts': 1, 'prob_t': 1})], [], []
+    beams, new_beams, results = [(1,
+                                  {'eos': 0, 'dec_inp': decoder_inputs, 'prob': 1, 'prob_ts': 1, 'prob_t': 1})], [], []
 
-    for dptr in range(len(decoder_inputs)-1):
-      if dptr > 0:
-        target_weights[dptr] = [1.]
-        beams, new_beams = new_beams[:beam_size], []
-      if debug: print("=====[beams]=====", beams)
-      heapq.heapify(beams)  # since we will srot and remove something to keep N elements
-      for prob, cand in beams:
-        if cand['eos']:
-          results += [(prob, cand)]
-          continue
-        
-        all_prob_ts = model_step(encoder_inputs, cand['dec_inp'], dptr, target_weights, bucket_id)
-        all_prob_t  = [0]*len(all_prob_ts)
-        all_prob    = all_prob_ts
+    for dptr in range(len(decoder_inputs) - 1):
+        if dptr > 0:
+            target_weights[dptr] = [1.]
+            beams, new_beams = new_beams[:beam_size], []
+        if debug: print("=====[beams]=====", beams)
+        heapq.heapify(beams)  # since we will srot and remove something to keep N elements
+        for prob, cand in beams:
+            if cand['eos']:
+                results += [(prob, cand)]
+                continue
 
-        # suppress copy-cat (respond the same as input)
-        if dptr < len(input_token_ids):
-          all_prob[input_token_ids[dptr]] = all_prob[input_token_ids[dptr]] * 0.01
+            all_prob_ts = model_step(encoder_inputs, cand['dec_inp'], dptr, target_weights, bucket_id)
+            all_prob_t = [0] * len(all_prob_ts)
+            all_prob = all_prob_ts
 
-        # beam search
-        for c in np.argsort(all_prob)[::-1][:beam_size]:
-          new_cand = {
-            'eos'     : (c == data_utils.EOS_ID),
-            'dec_inp' : [(np.array([c]) if i == (dptr+1) else k) for i, k in enumerate(cand['dec_inp'])],
-            'prob_ts' : cand['prob_ts'] * all_prob_ts[c],
-            'prob_t'  : cand['prob_t'] * all_prob_t[c],
-            'prob'    : cand['prob'] * all_prob[c],
-          }
-          new_cand = (new_cand['prob'], new_cand) # for heapq can only sort according to list[0]
+            # suppress copy-cat (respond the same as input)
+            if dptr < len(input_token_ids):
+                all_prob[input_token_ids[dptr]] = all_prob[input_token_ids[dptr]] * 0.01
 
-          if (len(new_beams) < beam_size):
-            heapq.heappush(new_beams, new_cand)
-          elif (new_cand[0] > new_beams[0][0]):
-            heapq.heapreplace(new_beams, new_cand)
+            # beam search
+            for c in np.argsort(all_prob)[::-1][:beam_size]:
+                new_cand = {
+                    'eos': (c == data_utils.EOS_ID),
+                    'dec_inp': [(np.array([c]) if i == (dptr + 1) else k) for i, k in enumerate(cand['dec_inp'])],
+                    'prob_ts': cand['prob_ts'] * all_prob_ts[c],
+                    'prob_t': cand['prob_t'] * all_prob_t[c],
+                    'prob': cand['prob'] * all_prob[c],
+                }
+                new_cand = (new_cand['prob'], new_cand)  # for heapq can only sort according to list[0]
+
+                if (len(new_beams) < beam_size):
+                    heapq.heappush(new_beams, new_cand)
+                elif (new_cand[0] > new_beams[0][0]):
+                    heapq.heapreplace(new_beams, new_cand)
 
     results += new_beams  # flush last cands
 
     # post-process results
     res_cands = []
     for prob, cand in sorted(results, reverse=True):
-      res_cands.append(cand)
+        res_cands.append(cand)
     return res_cands
 
-def gen_sample(sess ,gen_config, model, vocab, source_inputs, source_outputs, mc_search=True):
+
+def gen_sample(sess, gen_config, model, vocab, source_inputs, source_outputs, mc_search=True):
     sample_context = []
     sample_response = []
-    sample_labels =[]
+    sample_labels = []
     rep = []
-    #import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     for source_query, source_answer in zip(source_inputs, source_outputs):
         sample_context.append(source_query)
         sample_response.append(source_answer)

758251b91f5a9264bc07825504dc711af34e4491
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-05-02 16:47:12 -0700

    Fixed cmd line issue

--- al_neural_dialogue_train.py
+++ al_neural_dialogue_train.py
@@ -236,36 +236,34 @@ def al_train():
 
 def main(_):
     
- #   import argparse
- #   parser = argparse.ArgumentParser()
- #   parser.add_argument('train_type', type=str)
- #   parser.add_argument('--gen_file', type=str)
- #   parser.add_argument('--disc_file', type=str)
- #   args = parser.parse_args()
-
-
     seed = int(time.time())
     np.random.seed(seed)  
-   
-#    if args.train_type == 'disc': 
-#        print ("Runinng Discriminator Pre-Train") 
-#        disc_pre_train()
-#    elif args.train_type == 'gen':
-#        print ("Runinng Generator Pre-Train")
-#        gen_pre_train()
-#    else:
-#        print ("Runinng Adversarial")        
-#        al_train()
-#
-#    if (args.disc_file):
-#        conf.disc_config.train_data_file = args.disc_file
-#
-#    if (args.gen_file):
-#        conf.gen_config.train_data_file = args.gen_file
 
-  #  disc_pre_train()
-  #  gen_pre_train()
-    al_train()
+    
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('train_type', type=str)
+    parser.add_argument('--gen_file', type=str)
+    parser.add_argument('--disc_file', type=str)
+    args = parser.parse_args()
+
+    if (args.disc_file):
+        conf.disc_config.train_data_file = args.disc_file
+
+    if (args.gen_file):
+        conf.gen_config.train_data_file = args.gen_file
+ 
+    if args.train_type == 'disc': 
+        print ("Runinng Discriminator Pre-Train") 
+        disc_pre_train()
+    elif args.train_type == 'gen':
+        print ("Runinng Generator Pre-Train")
+        gen_pre_train()
+    else:
+        print ("Runinng Adversarial")        
+        al_train()
+
+   
 
 
 

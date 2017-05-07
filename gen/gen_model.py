bffa7658b689422c06f5ab6d4287fed03d270d46
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-05-06 20:47:13 -0700

    get batch return batch in original format

--- gen/gen_model.py
+++ gen/gen_model.py
@@ -274,70 +274,84 @@ class Seq2SeqModel(object):
           return outputs[0], outputs[1], outputs[2:]  # encoder_state, loss, outputs.
 
 
-    def get_batch(self, train_data, bucket_id, type=0):
+#    def get_batch(self, train_data, bucket_id, type=0):
+#       
+#        encoder_size, decoder_size        = self.buckets[bucket_id]
+#        encoder_inputs, decoder_inputs    = [], []
+#        batch_size                        = self.batch_size
+#        
+#        # Get a random batch of encoder and decoder inputs from data,
+#        # pad them if needed, reverse encoder inputs and add GO to decoder.
+#        batch_source_encoder, batch_source_decoder = [], []
+#        if type == 1:
+#            batch_size = 1
+#        for batch_i in xrange(batch_size):
+#            if type == 1:
+#                encoder_input, decoder_input = train_data[bucket_id]   
+#            elif type == 2:
+#                encoder_input_a, decoder_input = train_data[bucket_id][0]
+#                encoder_input = encoder_input_a[batch_i]
+#            elif type == 0:
+#                encoder_input, decoder_input = random.choice(train_data[bucket_id])
+#    
+#            batch_source_encoder.append(encoder_input)              # batch_source_encoder is a sampled column of queries (unpadded)
+#            batch_source_decoder.append(decoder_input)              # batch_source_decoder of answers, ended by EOS  
+#                                                             
+#            # Encoder inputs are padded and then reversed.
+#            encoder_pad = [data_utils.PAD_ID] * (encoder_size - len(encoder_input))
+#            encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
+#
+#            # Decoder inputs get an extra "GO" symbol, and are padded then.
+#            decoder_pad_size = decoder_size - len(decoder_input) - 1
+#            decoder_inputs.append([data_utils.GO_ID] + decoder_input +
+#                          [data_utils.PAD_ID] * decoder_pad_size)
+#
+#        # Now we create batch-major vectors from the data selected above.
+#        batch_encoder_inputs, batch_decoder_inputs, batch_weights = [], [], []
+#
+#        # Batch encoder inputs are just re-indexed encoder_inputs.
+#        for length_idx in xrange(encoder_size):
+#            batch_encoder_inputs.append(
+#                np.array([encoder_inputs[batch_idx][length_idx]
+#                  for batch_idx in xrange(batch_size)], dtype=np.int32))
+#        # at this point, batch_encoder_inputs are transposed, enc_size*batch_size
+#
+#        # Batch decoder inputs are re-indexed decoder_inputs, we create weights.
+#        for length_idx in xrange(decoder_size):
+#            batch_decoder_inputs.append(
+#                np.array([decoder_inputs[batch_idx][length_idx]
+#                    for batch_idx in xrange(batch_size)], dtype=np.int32))
+#
+#            # Each line is a word across the entire batch. Some of them are just pads, some are real
+#
+#            # Create target_weights to be 0 for targets that are padding.
+#            batch_weight = np.ones(batch_size, dtype=np.float32)
+#            for batch_idx in xrange(batch_size):
+#                # We set weight to 0 if the corresponding target is a PAD symbol.
+#                # The corresponding target is decoder_input shifted by 1 forward.
+#                if length_idx < decoder_size - 1:
+#                    target = decoder_inputs[batch_idx][length_idx + 1]
+#                if length_idx == decoder_size - 1 or target == data_utils.PAD_ID:
+#                    batch_weight[batch_idx] = 0.0
+#            batch_weights.append(batch_weight)
+#
+#
+#        # Returning: transposed/padded/GO/EOSed/ (x2  = source+target)  ||  weights as photographed || EOSed (x2 = source+target)
+#        return (batch_encoder_inputs, batch_decoder_inputs, batch_weights, batch_source_encoder, batch_source_decoder)
+
+    def get_batch(self, train_data, bucket_id):
         
-    
-        encoder_size, decoder_size        = self.buckets[bucket_id]
         encoder_inputs, decoder_inputs    = [], []
         batch_size                        = self.batch_size
         
         # Get a random batch of encoder and decoder inputs from data,
-        # pad them if needed, reverse encoder inputs and add GO to decoder.
         batch_source_encoder, batch_source_decoder = [], []
-        if type == 1:
-            batch_size = 1
         for batch_i in xrange(batch_size):
-            if type == 1:
-                encoder_input, decoder_input = train_data[bucket_id]   
-            elif type == 2:
-                encoder_input_a, decoder_input = train_data[bucket_id][0]
-                encoder_input = encoder_input_a[batch_i]
-            elif type == 0:
-                encoder_input, decoder_input = random.choice(train_data[bucket_id])
-    
+            encoder_input, decoder_input = random.choice(train_data[bucket_id])
+            
             batch_source_encoder.append(encoder_input)              # batch_source_encoder is a sampled column of queries (unpadded)
-            batch_source_decoder.append(decoder_input)              # batch_source_decoder of answers, ended by EOS  
-                                                             
-            # Encoder inputs are padded and then reversed.
-            encoder_pad = [data_utils.PAD_ID] * (encoder_size - len(encoder_input))
-            encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
-
-            # Decoder inputs get an extra "GO" symbol, and are padded then.
-            decoder_pad_size = decoder_size - len(decoder_input) - 1
-            decoder_inputs.append([data_utils.GO_ID] + decoder_input +
-                          [data_utils.PAD_ID] * decoder_pad_size)
-
+            batch_source_decoder.append(decoder_input)              # batch_source_decoder of answers, ended by EOS 
         
-
-        # Now we create batch-major vectors from the data selected above.
-        batch_encoder_inputs, batch_decoder_inputs, batch_weights = [], [], []
-
-        # Batch encoder inputs are just re-indexed encoder_inputs.
-        for length_idx in xrange(encoder_size):
-            batch_encoder_inputs.append(
-                np.array([encoder_inputs[batch_idx][length_idx]
-                  for batch_idx in xrange(batch_size)], dtype=np.int32))
-        # at this point, batch_encoder_inputs are transposed, enc_size*batch_size
-
-        # Batch decoder inputs are re-indexed decoder_inputs, we create weights.
-        for length_idx in xrange(decoder_size):
-            batch_decoder_inputs.append(
-                np.array([decoder_inputs[batch_idx][length_idx]
-                    for batch_idx in xrange(batch_size)], dtype=np.int32))
-
-            # Each line is a word across the entire batch. Some of them are just pads, some are real
-
-            # Create target_weights to be 0 for targets that are padding.
-            batch_weight = np.ones(batch_size, dtype=np.float32)
-            for batch_idx in xrange(batch_size):
-                # We set weight to 0 if the corresponding target is a PAD symbol.
-                # The corresponding target is decoder_input shifted by 1 forward.
-                if length_idx < decoder_size - 1:
-                    target = decoder_inputs[batch_idx][length_idx + 1]
-                if length_idx == decoder_size - 1 or target == data_utils.PAD_ID:
-                    batch_weight[batch_idx] = 0.0
-            batch_weights.append(batch_weight)
+        return batch_source_encoder, batch_source_decoder
 
 
-        # Returning: transposed/padded/GO/EOSed/ (x2  = source+target)  ||  weights as photographed || EOSed (x2 = source+target)
-        return (batch_encoder_inputs, batch_decoder_inputs, batch_weights, batch_source_encoder, batch_source_decoder)

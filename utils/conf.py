9494f2e4cc6930463cf165fdd164f43a13b4ecdb
kstats <katstasaski@gmail.com>
2017-05-06 19:08:55 -0700

    Added only pickling every n steps

--- utils/conf.py
+++ utils/conf.py
@@ -7,7 +7,7 @@ class disc_config(object):
     lr_decay                    = 0.6
     max_grad_norm               = 5    
     keep_prob                   = 1.  
-    batch_size                  = 128
+    batch_size                  = 5
     embed_dim                   = 128
     class_num                   = 2
     init_scale                  = 0.1    
@@ -27,6 +27,9 @@ class disc_config(object):
     iters                       = 1
    # valid_num                   = 100
 
+    #after how many global steps should we pickle?
+    plot_every                  = 1
+
 class gen_config(object):
     
     num_layers                  = 1
@@ -35,7 +38,7 @@ class gen_config(object):
     learning_rate_decay_factor  = 0.99
     max_gradient_norm           = 5.0
     keep_prob                   = 1.
-    batch_size                  = 128
+    batch_size                  = 5
     beam_size                   = 5
 
     vocab_path                  = './data/movie_25000'
@@ -50,6 +53,6 @@ class gen_config(object):
     steps_per_checkpoint        = 8000
     steps_per_sample            = 150
     
-    iters                       = 1000
+    iters                       = 1
     force_iters                 = 0
     buckets                     = [(5, 10), (10, 15), (20, 25), (40, 50), (50, 50)]

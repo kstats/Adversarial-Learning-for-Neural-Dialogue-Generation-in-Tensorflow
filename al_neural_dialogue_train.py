14a742d4f9f3d0ab2ac488f786b93144a6e6e73b
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-05-06 21:15:27 -0700

    Backward compatible

--- al_neural_dialogue_train.py
+++ al_neural_dialogue_train.py
@@ -12,9 +12,21 @@ gen_config  = conf.gen_config
 disc_config = conf.disc_config
 evl_config  = conf.disc_config
 
-# prepare data for discriminator and generator
-def disc_train_data(source_inputs, source_outputs, sample_context, sample_response, sample_labels, responses, mc_search=False, isDisc=True, temp=True):
-    
+
+# pre train generator
+def gen_pre_train():
+    gens.train(gen_config)
+
+    # prepare data for discriminator and generator
+def disc_train_data(sess, gen_model, vocab, source_inputs, source_outputs, gen_inputs, gen_outputs, mc_search=False, isDisc=True, temp=True):
+    # sample_context2, sample_response2, sample_labels2, responses2 = gens.gen_sample(sess, gen_config, gen_model, vocab,
+    #                                             gen_inputs, gen_outputs, mc_search=mc_search)
+    sample_context, sample_response, sample_labels, responses = gens.gen_guided_sample(sess, gen_inputs, gen_outputs, gen_config, gen_model, vocab)
+
+    #for n in range(len(sample_response)):
+     #   if n % 2 == 1:
+      #      sample_response[n] = [sample_response[n]]
+    #responses = [responses]
     print("disc_train_data, mc_search: ", mc_search)
     rem_set = []
     for i in range(len(sample_labels)):
@@ -25,6 +37,7 @@ def disc_train_data(source_inputs, source_outputs, sample_context, sample_respon
         del sample_context[elem]
         del sample_response[elem]
         del sample_labels[elem]
+        # del responses[elem]
 
     dataset = {}
     dataset['is_disc']  = True
@@ -49,15 +62,28 @@ def disc_train_data(source_inputs, source_outputs, sample_context, sample_respon
         for resp in sample_response:
             dataset['response'].append(resp[0])
 
+    # dataset['len'] = (len(source_inputs)-1) + len(sample_context) if isDisc else len(sample_context)
 
-    train_inputs, train_labels, train_masks = data_util.convert_to_format(data_util.dataset_padding(dataset, disc_config.max_len))
+
+    '''resp = []
+    for input, response, label in zip(sample_context, sample_response, sample_labels):
+       print(str(label) + "\t" + str(input) + "\t" + str(response))
+       resp.append(response)
+    sample_inputs = zip(sample_context, sample_response)
+    def len_argsort(seq):
+        return sorted(range(len(seq)), key=lambda x: len(seq[x]))
+    sorted_index = len_argsort(sample_inputs)'''
+
+
+    '''train_set_x = []
+    train_set_y = []
+    train_set_x = [sample_context[i] for i in sorted_index]
+    train_set_y = [sample_labels[i] for i in sorted_index]
+    train_set=(train_set_x,train_set_y)'''
+
+    train_inputs, train_labels, train_masks = data_utils.convert_to_format(data_utils.dataset_padding(dataset, disc_config.max_len))
 
     return train_inputs, train_labels, train_masks, responses
-
-
-# pre train generator
-def gen_pre_train():
-    gens.train(gen_config)
 
 def gen_pre_train2():
     gen_config.batch_size = 1
@@ -149,6 +175,136 @@ def disc_pre_train():
                 print("Saved model chechpoint to{}\n".format(path))
 
 
+# Adversarial Learning for Neural Dialogue Generation
+def al_train():
+    # gen_config.batch_size = 1
+    with tf.Session() as sess:
+        train_summary_dir = os.path.join(disc_config.out_dir, "summaries", "train")
+        train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+        initializer = tf.random_uniform_initializer(-1 * disc_config.init_scale, 1 * disc_config.init_scale)
+        with tf.variable_scope("model", reuse=None, initializer=initializer):
+            disc_model = discs.create_model(sess, disc_config, is_training=True)
+        gen_model = gens.create_model(sess, gen_config)
+        vocab, rev_vocab, dev_set, train_set = data_utils.prepare_data(gen_config)
+        train_bucket_sizes = [len(train_set[b]) for b in xrange(len(gen_config.buckets))]
+        train_total_size = float(sum(train_bucket_sizes))
+        train_buckets_scale = [sum(train_bucket_sizes[:i + 1]) / train_total_size
+                               for i in xrange(len(train_bucket_sizes))]
+
+        rewards = []
+        steps = []
+        perplexity = []
+        disc_loss = []
+        disc_steps = []
+        gen_steps = []
+        gstep = 0
+        while True:            
+            gstep += 1
+            steps.append(gstep)
+            for i in range(disc_config.iters):
+                bucket_id = min([j for j in xrange(len(train_buckets_scale))
+                                 if train_buckets_scale[j] > np.random.random_sample()])
+                print("===========================Update Discriminator %d.%d=============================" % (gstep, i))
+                # 1.Sample (X,Y) from real data
+                _, _, _, source_inputs, source_outputs = gen_model.get_batch(train_set, bucket_id, 0)
+                # 1.5 sample x,y from data to generate samples from
+                _, _, _, gen_inputs, gen_outputs = gen_model.get_batch(train_set, bucket_id, 0)
+                # 2.Sample (X,Y) and (X, ^Y) through ^Y ~ G(*|X)
+                train_inputs, train_labels, train_masks, _ = disc_train_data(sess, gen_model, vocab,
+                                                                             source_inputs, source_outputs, gen_inputs,
+                                                                             gen_outputs, mc_search=False, isDisc=True,
+                                                                             temp=True)
+                # 3.Update D using (X, Y ) as positive examples and(X, ^Y) as negative examples
+                print('Discriminator input/labels:')
+                print(train_inputs)
+                print(train_labels)
+                disc_steps.append((gstep-1) * disc_config.iters + i)
+                disc_l = disc_step(sess, disc_model, train_inputs, train_labels, train_masks)
+                disc_loss.append(disc_l)
+
+
+            for i in range(gen_config.iters):
+                small_step = (gstep - 1) * gen_config.iters + i
+                gen_steps.append(small_step)
+                bucket_id = min([j for j in xrange(len(train_buckets_scale))
+                                    if train_buckets_scale[j] > np.random.random_sample()])
+
+                print("===============================Update Generator %d.%d=============================" % (gstep, i))
+                # 1.Sample (X,Y) from real data
+                update_gen_data = gen_model.get_batch(train_set, bucket_id, 0)
+                encoder, decoder, weights, source_inputs, source_outputs = update_gen_data
+                import pdb; pdb.set_trace()
+                test = gens.sample_from(sess,np.asarray(encoder),bucket_id,gen_config,gen_model,vocab)
+                # 2.Sample (X,Y) and (X, ^Y) through ^Y ~ G(*|X) with Monte Carlo search
+                # train_inputs, train_labels, train_masks, responses = disc_train_data(sess,gen_model,vocab,
+                #                                             source_inputs,source_outputs,source_inputs,source_outputs, mc_search=True, isDisc=False)
+                train_inputs, train_labels, train_masks, responses = disc_train_data(sess, gen_model, vocab,
+                                                                                     source_inputs, source_outputs,
+                                                                                     source_inputs, source_outputs,
+                                                                                     mc_search=False, isDisc=False,
+                                                                                     temp=False)
+                # 3.Compute Reward r for (X, ^Y ) using D.---based on Monte Carlo search
+                reward = disc_step(sess, disc_model, train_inputs, train_labels, train_masks, do_train=False)
+                #import pdb; pdb.set_trace()
+                #rew = tf.summary.scalar("reward", tf.reduce_mean(reward[:,1]))
+                #summ = sess.run(rew)
+                #train_summary_writer.add_summary(summ, small_step)
+                #rew2 = tf.summary.scalar("reward2", tf.reduce_mean(reward[:, 1]).eval())
+                #sum2 = sess.run(rew2)
+                #train_summary_writer.add_summary(sum2, small_step)
+                print("Step %d, here are the discriminator inputs/labels on which we calculate reward:" % gstep)
+                print(train_inputs)
+                print(train_labels)
+                print("Step %d, here are the discriminator logits:" % gstep)
+                print(reward)
+                rewards.append(np.mean(reward[:,1]))
+                # 4.Update G on (X, ^Y ) using reward r
+                # import pdb; pdb.set_trace()
+                decoder_inputs = []
+                for res in responses:
+                    dec_gen = [data_utils.GO_ID] + res[:gen_config.buckets[bucket_id][1]]
+                    if len(dec_gen) < gen_config.buckets[bucket_id][1]:
+                        dec_gen = dec_gen + [0] * (gen_config.buckets[bucket_id][1] - len(dec_gen))
+                    dec_gen = np.reshape(dec_gen, (-1, 1))
+                    decoder_inputs.append(dec_gen)
+                # import pdb; pdb.set_trace()
+                decoder_inputs = np.transpose(np.asarray(decoder_inputs))
+                decoder_inputs = np.squeeze(decoder_inputs)
+                gen_model.step(sess, encoder, decoder_inputs, weights, bucket_id, mode=gen_model.SM_POLICY_TRAIN,
+                               reward=reward[:, 1])
+
+            '''dec_gen = []
+            for i in range(len(responses)):
+                dec_gen.append(responses[i][:gen_config.buckets[bucket_id][1][0]])
+                if len(dec_gen)< gen_config.buckets[bucket_id][1]:
+                    dec_gen = dec_gen + [0]*(gen_config.buckets[bucket_id][1] - len(dec_gen))
+            dec_gen = np.reshape(dec_gen, (-1,gen_config.batch_size,1))'''
+
+            # 5.Teacher-Forcing: Update G on (X, Y )
+
+            for i in range(gen_config.force_iters):
+                print("===========================Force Generator %d.%d=============================" % (gstep, i))
+                _, loss, _ = gen_model.step(sess, encoder, decoder, weights, bucket_id, mode=gen_model.SM_TRAIN)
+
+                #print("loss: ", loss)
+                perplexity.append(loss)
+                # pickle.dump(perplexity, open("perplexity.p", "wb"))
+            if gstep % disc_config.plot_every:
+                pickle.dump(disc_loss, open("disc_loss.p", "wb"))
+                pickle.dump(disc_steps, open("disc_steps.p", "wb"))
+                pickle.dump(steps, open("steps.p", "wb"))
+                pickle.dump(rewards, open("rewards.p", "wb"))
+                pickle.dump(perplexity, open("perplexity.p", "wb"))
+                pickle.dump(gen_steps, open("gen_steps.p", "wb"))
+
+        # add checkpoint
+        checkpoint_dir = os.path.abspath(os.path.join(disc_config.out_dir, "checkpoints"))
+        checkpoint_prefix = os.path.join(checkpoint_dir, "disc.model")
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        pass
+
 
 def sample_bucket_id(train_buckets_scale):
     return min([j for j in xrange(len(train_buckets_scale))
@@ -156,7 +312,7 @@ def sample_bucket_id(train_buckets_scale):
 
 
 # Adversarial Learning for Neural Dialogue Generation
-def al_train():
+def _al_train():
 
     with tf.Session() as sess:
         
@@ -204,16 +360,15 @@ def al_train():
                 
                 print("===========================Update Discriminator %d.%d=============================" % (gstep, i))
                 # 1.Sample (X,Y) from real data
-                source_inputs, source_outputs           = gen_model.get_batch(train_set, bucket_id)
+                source_inputs, source_outputs           = gen_model._get_batch(train_set, bucket_id)
 
                 # 1.5 sample x,y from data to generate samples from
-                gen_inputs, gen_outputs                 = gen_model.get_batch(train_set, bucket_id)
+                gen_inputs, gen_outputs                 = gen_model._get_batch(train_set, bucket_id)
                 
                 encoder_inputs, decoder_inputs, _       = data_utils.src_to_gen(gen_inputs, gen_outputs, gen_config.buckets, bucket_id, gen_model.batch_size)
                 
                 # 2.Sample (X,Y) and (X, ^Y) through ^Y ~ G(*|X)
-                #TODO Replace with Gil's function
-                sample_responses                         = gens.gen_guided_sample(sess, encoder_inputs, decoder_inputs, gen_config, gen_model, vocab) 
+                sample_responses                         = gens.sample_from(sess, encoder_inputs, bucket_id, gen_config, gen_model, vocab)
                 
                 mixed_encoder  = list(np.concatenate((source_inputs, gen_inputs)))
                 mixed_decoder  = list(np.concatenate((source_outputs, sample_responses)))
@@ -238,12 +393,12 @@ def al_train():
 
                 print("===============================Update Generator %d.%d=============================" % (gstep, i))
                 # 1.Sample (X,Y) from real data
-                source_inputs, source_outputs               = gen_model.get_batch(train_set, bucket_id, 0)
+                source_inputs, source_outputs               = gen_model._get_batch(train_set, bucket_id, 0)
 
                 encoder, decoder, weights                   = data_utils.src_to_gen(source_inputs, source_outputs, gen_config.buckets, bucket_id, gen_model.batch_size) 
 
                 # 2.Sample (X,Y) and (X, ^Y) through ^Y ~ G(*|X) with Monte Carlo search
-                sample_response                             = gens.gen_guided_sample(sess, encoder, decoder, gen_config, gen_model, vocab)
+                sample_response                             = gens.sample_from(sess, encoder, bucket_id, gen_config, gen_model, vocab)
 
                 sample_labels                               = [0]*gen_model.batch_size
 
@@ -283,7 +438,6 @@ def al_train():
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
         pass
-
 
 
 
